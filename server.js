e32e9c735b19947439e8e4e4e94f5c98653a1349
M Imam Pratama <imambungo@gmail.com>
2022-08-22 09:41:14 +0700

    rename app to server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // https://expressjs.com/en/starter/hello-world.html
 const express = require('express') // selagi official docs masih make require, gk usah make ES import
-const app = express()
+const server = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server listening on port ${port} (localhost:${port})`)
 })
