2955851d26402ed68eaed61bbf9b4e22c19f0be8
M Imam Pratama <imambungo@gmail.com>
2022-08-23 11:25:16 +0700

    tes cron job

--- server.js
+++ server.js
@@ -10,3 +10,27 @@ server.get('/', (req, res) => {
 server.listen(port, () => {
     console.log(`server listening on port ${port} (localhost:${port})`)
 })
+
+
+// https://www.npmjs.com/package/node-cron
+var cron = require('node-cron');
+
+let task1 = cron.schedule('* * * * *', () => {
+    console.log('running a task 1 every minute');
+}, {
+    scheduled: false
+});
+
+let task2 = cron.schedule('* * * * *', () => {
+    console.log('running another task 2 every minute');
+});
+
+server.get('/start-task1', (req, res) => {
+    res.send('starting task 1')
+    task1.start()
+})
+
+server.get('/stop-task2', (req, res) => {
+    res.send('stopping task 2')
+    task2.stop()
+})
