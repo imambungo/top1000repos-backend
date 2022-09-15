2b6b5fef1707a806af1ddd3fdf1e7acd1152286f
M Imam Pratama <imambungo@gmail.com>
2022-09-15 13:20:26 +0700

    x

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ let task1 = cron.schedule('* * * * *', () => { // 0 12 15 * *
 
 server.get('/a', async (req, res) => {
     const data = await fetchRepos(1)
-    const full_name = data.items[0].full_name
+    let index = 0
+    let repo = data.items[index]
+    updateOrInsertRepo(repo)
+
     res.send(full_name) // data (json) bisa jg
 })
 
@@ -33,6 +36,18 @@ const fetchRepos = async (page) => {
     const response = await fetch(`https://api.github.com/search/repositories?q=stars%3A%3E1000&sort=stars&page=${page}&per_page=100`);
     const data = await response.json();
     return data
+}
+
+const updateOrInsertRepo = async (repo) => {
+    const id = repo.id
+    const full_name = repo.full_name
+    const owner_avatar_url = repo.owner.avatar_url
+    const html_url = repo.html_url
+    const description = repo.description
+    const last_commit_date = repo.pushed_at.slice(0, 10) // slice 2022-09-14 from 2022-09-14T23:19:32Z
+    const stargazers_count = repo.stargazers_count
+    const license_key = repo.license.key
+    const last_verified_at = new Date().toISOString().slice(0, 10) // https://stackoverflow.com/a/35922073/9157799
 }
 
 
