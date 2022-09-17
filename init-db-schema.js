aa5fd36ef607c326dcc1c0a39c471026059d9d80
M Imam Pratama <imambungo@gmail.com>
2022-09-17 21:59:55 +0700

    s

--- init-db-schema.js
+++ init-db-schema.js
@@ -2,3 +2,4 @@
 import sql from './db.js' // db.js is not version controlled
 
 await sql.file('github top repos.sql')
+throw 'SUCCESS! THIS IS NOT AN ERROR!' // creating database instance cause the script to persist | https://stackoverflow.com/a/7223319/9157799
