ff30d177323b2999945cf97c57e0050348ab2a3f
M Imam Pratama <imambungo@gmail.com>
2022-10-02 13:42:09 +0700

    s

--- init-db-schema.js
+++ init-db-schema.js
@@ -1,5 +1,10 @@
 // https://github.com/porsager/postgres#usage
 import sql from './db.js' // db.js is not version controlled
 
-await sql.file('github top repos.sql')
+await sql.file('github top repos.sql') // the schema
+await sql`
+	INSERT INTO fetch_task (name)
+	       VALUES ('repo'), ('top 5 pr'), ('top 5 issues');
+`
+
 throw 'SUCCESS! THIS IS NOT AN ERROR!' // creating database instance cause the script to persist | https://stackoverflow.com/a/7223319/9157799
