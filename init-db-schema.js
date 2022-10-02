f07fc378b49f860268db74a72a3bdfd97e7ee217
M Imam Pratama <imambungo@gmail.com>
2022-10-02 17:20:32 +0700

    s

--- init-db-schema.js
+++ init-db-schema.js
@@ -3,8 +3,11 @@ import sql from './db.js' // db.js is not version controlled
 
 await sql.file('github top repos.sql') // the schema
 await sql`
-	INSERT INTO fetch_task (name)
-	       VALUES ('repo'), ('top 5 pr'), ('top 5 issues');
+	INSERT INTO standalone_data
+	       VALUES ('server_last_active_date', '2000-01-01'),
+			        ('repo_daily_fetch_count', '0'),
+					  ('top_5_pr_daily_fetch_count', '0'),
+					  ('top_5_issues_daily_fetch_count', '0');
 `
 
 throw 'SUCCESS! THIS IS NOT AN ERROR!' // creating database instance cause the script to persist | https://stackoverflow.com/a/7223319/9157799
