594a82f2a78fe23e3f22543888d75a848959088d
M Imam Pratama <imambungo@gmail.com>
2023-01-24 18:24:04 +0700

    fix

--- src/script/reset-fetch-count.js
+++ src/script/reset-fetch-count.js
@@ -3,8 +3,8 @@ import sql from './../config/db.js'
 import persistent_global_variable from './../lib/persistent_global_variable.js'
 const pgv = persistent_global_variable(sql)
 
-pgv.set('repo_daily_fetch_count', 0)
-pgv.set('top_5_pr_daily_fetch_count', 0)
-pgv.set('top_5_issues_daily_fetch_count', 0)
+await pgv.set('repo_daily_fetch_count', 0) // the awaits are important to make sure the code finishes before below exception is thrown
+await pgv.set('top_5_pr_daily_fetch_count', 0)
+await pgv.set('top_5_issues_daily_fetch_count', 0)
 
 throw 'SUCCESS! THIS IS NOT AN ERROR!' // creating database instance cause the script to persist | https://stackoverflow.com/a/7223319/9157799
