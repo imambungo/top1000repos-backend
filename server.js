fd1800c2ff4ab2beeae902f76d98557c6d26d3b2
M Imam Pratama <imambungo@gmail.com>
2023-01-27 17:35:51 +0700

    s

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.get('/repositories', async (req, res) => {
 		SELECT
 			id, full_name, html_url, description, last_commit_date, stargazers_count, archived, topics, last_verified_at, num_of_closed_pr_since_1_year, open_issues_count, total_thumbs_up_of_top_5_closed_pr_since_1_year, total_thumbs_up_of_top_5_closed_issues_since_1_year, total_thumbs_up_of_top_5_open_issue_of_all_time, has_issues
 		FROM repository;
-	` // TODO: consider a dedicated top_5_pr_thumbs_up column in repository
+	`
 	repos = repos.map(repo => ({ // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 		...repo, // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
 		last_commit_date: repo.last_commit_date.toISOString().slice(0, 10), // "2022-10-18T00:00:00.000Z" -> "2022-10-18" | https://stackoverflow.com/a/35922073/9157799
