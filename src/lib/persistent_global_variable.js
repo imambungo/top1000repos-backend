2129c4f2ee070246e3038d2745e611aafa739f28
M Imam Pratama <imambungo@gmail.com>
2023-02-27 23:43:35 +0700

    sori whatever

--- src/lib/persistent_global_variable.js
+++ src/lib/persistent_global_variable.js
@@ -1,7 +1,8 @@
 const persistent_global_variable = sql => ({
    get: async name => {
       const [{ value }] = await sql`SELECT value FROM persistent_global_variable WHERE name = ${name}` // https://github.com/porsager/postgres#usage
-      console.log(JSON.parse(value))
+      console.log(value)
+      console.log(`json parse ${JSON.parse(value)}`)
       return JSON.parse(value)
    },
    set: async (name, value) => await sql`UPDATE persistent_global_variable SET value = ${JSON.stringify(value)} WHERE name = ${name}`,
