a9b496f4fcd22e51ff746d925c0386818c213689
M Imam Pratama <imambungo@gmail.com>
2023-02-28 00:10:03 +0700

    s

--- src/script/init-db-schema.js
+++ src/script/init-db-schema.js
@@ -4,7 +4,7 @@ import sql from './../config/db.js'
 await sql.file('./src/github top repos.sql') // the schema | use path relative to project root | https://stackoverflow.com/q/70265259/9157799
 await sql`
 	INSERT INTO persistent_global_variable
-	       VALUES ('server_last_active_date', '"2000-01-01"'),
+	       VALUES ('server_last_active_date', '"2000-01-01"'), -- be careful, make sure to double quote strings so it's a valid JSON
 			        ('repo_daily_fetch_count', '0'),
 					  ('top_5_closed_pr_daily_fetch_count', '0'),
 					  ('top_5_closed_issues_daily_fetch_count', '0'),
