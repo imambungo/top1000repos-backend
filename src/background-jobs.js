e999a24e3540520fe17132a2350298eb585a3b8f
M Imam Pratama <imambungo@gmail.com>
2023-05-25 13:32:09 +0700

    s

--- src/background-jobs.js
+++ src/background-jobs.js
@@ -111,7 +111,7 @@ let taskFetchGithubApi = Cron('*/9 * * * * *', { timezone: 'Etc/UTC' }, async ()
                error_message += `\nrepo_full_name: ${repo_full_name}`
                error_message += `\ndate          : ${date}`
                error_message += `\nurl           : ${url}`
-               error_message += `\nJSON          : ${data}`
+               error_message += `\nJSON          : ${data.toString()}`
                throw error_message
             }
             return data
