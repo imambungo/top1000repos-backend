113c014adf58a688c040f5e0e1fe484a5d4913a9
M Imam Pratama <imambungo@gmail.com>
2023-05-25 13:46:09 +0700

    s

--- src/background-jobs.js
+++ src/background-jobs.js
@@ -76,19 +76,20 @@ let taskFetchGithubApi = Cron('*/9 * * * * *', { timezone: 'Etc/UTC' }, async ()
       } else if (await pgv.get('top_5_closed_pr_daily_fetch_count') < 1000) { // fetch top 5 CLOSED PR and stuff
          const fetch_top_5_closed_PR_since = async (repo_full_name, date) => { // fetch top 5 closed PR of the last 365 days
             const url = `https://api.github.com/search/issues?sort=reactions-%2B1&per_page=5&q=state:closed%20type:pr%20closed:%3E${date}%20repo:${repo_full_name}`
-            try {
-               const response = await fetch(url, fetchOptions) // https://trello.com/c/aPVztlM3/8-fetch-api-get-top-5-closed-possibly-merged-prs-of-the-last-12-months
-               const data = await response.json()
-               return data
-            } catch (e) {
-               console.log('-------------------')
-               console.log(e)
-               let exception_message = 'CUSTOM EXCEPTION fetch_top_5_closed_PR_since()'
-               exception_message += `\nrepo_full_name: ${repo_full_name}`
-               exception_message += `\ndate          : ${date}`
-               exception_message += `\nurl           : ${url}`
-               throw exception_message
+            const response = await fetch(url, fetchOptions) // https://trello.com/c/aPVztlM3/8-fetch-api-get-top-5-closed-possibly-merged-prs-of-the-last-12-months
+            const data = await response.json()
+            if (!response.ok) { // https://stackoverflow.com/a/38236296/9157799
+               let error_message = 'ERROR'
+               error_message += '---------------------------------'
+               error_message += 'fetch_top_5_closed_PR_since()'
+               error_message += `\nrepo_full_name: ${repo_full_name}`
+               error_message += `\ndate          : ${date}`
+               error_message += `\nurl           : ${url}`
+               error_message += `\nJSON          : ${JSON.stringify(data, null, 2)}` // https://stackoverflow.com/q/5612787/9157799#comment53474797_5612849
+               error_message += '---------------------------------'
+               throw error_message
             }
+            return data
          }
 
          const repo_number = await pgv.get('top_5_closed_pr_daily_fetch_count') + 1
