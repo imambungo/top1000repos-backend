5e8c2925eb448d3ac1221efdea017a8dd7d1a527
M Imam Pratama <imambungo@gmail.com>
2025-09-04 11:40:22 +0700

    s

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,9 @@ import * as dotenv from 'dotenv' // see https://www.npmjs.com/package/dotenv#usa
 dotenv.config()
 
 import postgres from 'postgres' // https://github.com/porsager/postgres
-const sql = postgres() // https://github.com/porsager/postgres#postgresurl-options | will fall back to psql environment variables: https://github.com/porsager/postgres#environmental-variables | https://www.postgresql.org/docs/current/libpq-envars.html
+const sql = postgres({
+   idle_timeout: 60 * 5   // ECONNRESET fix/workaround | https://github.com/porsager/postgres/issues/179 | https://github.com/porsager/postgres?tab=readme-ov-file#connection-timeout
+}) // https://github.com/porsager/postgres#postgresurl-options | Options will fall back to psql environment variables: https://github.com/porsager/postgres#environmental-variables | https://www.postgresql.org/docs/current/libpq-envars.html
 
 await sql`SELECT 1;` // if the password is wrong, throw error as early as possible. Postgres.js doesn't check the validity of the options right away. | https://stackoverflow.com/a/3670000/9157799
 
