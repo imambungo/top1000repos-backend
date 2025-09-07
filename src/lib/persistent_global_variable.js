8e98b8fcbb3cd7dacaab6aa8af27897b274e8ec7
M Imam Pratama <imambungo@gmail.com>
2025-09-07 14:27:21 +0700

    try try catch

--- src/lib/persistent_global_variable.js
+++ src/lib/persistent_global_variable.js
@@ -1,7 +1,40 @@
+// TEMPORARY
+const sendToTelegram = async (message) => { // https://core.telegram.org/bots/api#sendmessage
+	const requestBody = {
+		'chat_id': process.env.TELEGRAM_USER_ID,
+		'text': message,
+		'parse_mode': 'HTML' // https://core.telegram.org/bots/api#formatting-options | https://stackoverflow.com/a/49538689/9157799
+	}
+	try { // handle ConnectTimeoutError
+		const response = await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_API_TOKEN}/sendMessage`, {
+			 method: 'POST',
+			 body: JSON.stringify(requestBody),
+			 headers: { 'Content-Type': 'application/json' }
+		})
+		const data = await response.json()
+		if (!response.ok) { // if API/HTTP error | https://stackoverflow.com/a/38236296/9157799
+			console.log(`API error:\n${response.status} ${JSON.stringify(data, null, 2)}`) // https://stackoverflow.com/q/5612787/9157799#comment53474797_5612849
+		}
+	} catch (err) {
+		console.log(`Failed to fetch to telegram API:\n${err}`)
+	}
+}
+
 const persistent_global_variable = sql => ({
    get: async name => {
-      const [{ value }] = await sql`SELECT value FROM persistent_global_variable WHERE name = ${name}` // https://github.com/porsager/postgres#usage
-      return JSON.parse(value)
+      try { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+         const [{ value }] = await sql`SELECT value FROM persistent_global_variable WHERE name = ${name}` // https://github.com/porsager/postgres#usage
+         return JSON.parse(value)
+      } catch (error) {
+         if (error.message.includes("ECONNRESET")) { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#instance_properties
+            await sql.end()
+            const [{ value }] = await sql`SELECT value FROM persistent_global_variable WHERE name = ${name}` // https://github.com/porsager/postgres#usage
+            await sendToTelegram('ECONNRESET survived') // TEMPORARY
+            return JSON.parse(value)
+         } else {
+            console.error(error) // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
+         }
+      }
    },
    set: async (name, value) => await sql`UPDATE persistent_global_variable SET value = ${JSON.stringify(value)} WHERE name = ${name}`,
    increment: async name => await sql`UPDATE persistent_global_variable SET value = value::int + 1 WHERE name = ${name}` // https://stackoverflow.com/q/10233298/9157799#comment17889893_10233360
