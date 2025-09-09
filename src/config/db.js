d8cf339c84083ed3428ad41ea6c5df78a48f5e07
M Imam Pratama <imambungo@gmail.com>
2025-09-10 00:48:44 +0700

    🤞

--- src/config/db.js
+++ src/config/db.js
@@ -2,16 +2,33 @@ import * as dotenv from 'dotenv' // see https://www.npmjs.com/package/dotenv#usa
 dotenv.config()
 
 import postgres from 'postgres' // https://github.com/porsager/postgres
-let sql = postgres() // https://github.com/porsager/postgres#postgresurl-options | Options will fall back to psql environment variables: https://github.com/porsager/postgres#environmental-variables | https://www.postgresql.org/docs/current/libpq-envars.html
 
-await sql`SELECT 1;` // if the password is wrong, throw error as early as possible. Postgres.js doesn't check the validity of the options right away. | https://stackoverflow.com/a/3670000/9157799
+const create_sql_wrapper = async () => { // because of ECONNRESET | https://github.com/porsager/postgres/issues/179
+   const pass_by_reference = {
+      sql: postgres() // https://github.com/porsager/postgres#postgresurl-options | Options will fall back to psql environment variables: https://github.com/porsager/postgres#environmental-variables | https://www.postgresql.org/docs/current/libpq-envars.html
+   }
+   await pass_by_reference.sql`SELECT 1;` // if the password is wrong, throw error as early as possible. Postgres.js doesn't check the validity of the options right away. | https://stackoverflow.com/a/3670000/9157799
 
-const reinstantiate = async () => { // to be called when ECONNRESET is caught in a try-catch | https://github.com/porsager/postgres/issues/179
-   await sql.end()
-   sql = postgres() // https://github.com/porsager/postgres?tab=readme-ov-file#connection_ended
-   sql.reinstantiate = reinstantiate
+   const sql_wrapper = async (strings, ...expressions) => { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
+      return await pass_by_reference.sql(strings, ...expressions) // FYI: the async and await can be omitted
+   }
+
+   // search in IDE for "sql."
+   sql_wrapper.file = pass_by_reference.sql.file
+   sql_wrapper.end  = pass_by_reference.sql.end
+
+   sql_wrapper.reinstantiate = async () => { // to be called when ECONNRESET is caught in a try-catch | https://github.com/porsager/postgres/issues/179
+      await pass_by_reference.sql.end()
+      pass_by_reference.sql = postgres() // https://github.com/porsager/postgres?tab=readme-ov-file#connection_ended
+
+      // search in IDE for "sql."
+      sql_wrapper.file = pass_by_reference.sql.file
+      sql_wrapper.end  = pass_by_reference.sql.end
+   }
+
+   return sql_wrapper
 }
 
-sql.reinstantiate = reinstantiate
+const sql = await create_sql_wrapper()
 
 export default sql
