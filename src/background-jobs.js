53e99d403d2273a65b5702c17bec3e7235d1fea0
M Imam Pratama <imambungo@gmail.com>
2025-09-28 11:51:17 +0700

    s

--- src/background-jobs.js
+++ src/background-jobs.js
@@ -41,27 +41,6 @@ let task_fetch_github_api = Cron('*/9 * * * * *', { timezone: 'Etc/UTC' }, async
             if (num_of_repos < 1000) pgv.set('repo_daily_fetch_count', 0)                 // handle edge case when a repo at the start or end of page gained or lost rank by 1
          }
       } else if (await pgv.get('top_5_closed_pr_daily_fetch_count') < 1000) { // fetch top 5 CLOSED PR and stuff
-         const fetch_top_5_closed_PR_since = async (repo_full_name, date) => { // fetch top 5 closed PR of the last 365 days
-            const url = `https://api.github.com/search/issues?sort=reactions-%2B1&per_page=5&q=state:closed%20type:pr%20closed:%3E${date}%20repo:${repo_full_name}`
-            const response = await fetch(url, github_api_fetch_options) // https://trello.com/c/aPVztlM3/8-fetch-api-get-top-5-closed-possibly-merged-prs-of-the-last-12-months
-            const data = await response.json()
-            if (!response.ok) { // https://stackoverflow.com/a/38236296/9157799
-               // MAYBE TODO: When
-               //               "The listed users and repositories cannot be searched either because the resources do not exist or you do not have permission to view them."
-               //             then delete the repo from the database. Restarting the app won't remove it from the db unless it's been 24 hours. See clear_outdated_repos()
-               let error_message = 'ERROR'
-               error_message += '\n------------------------------'
-               error_message += '\nfetch_top_5_closed_PR_since()'
-               error_message += `\nrepo_full_name: ${repo_full_name}`
-               error_message += `\ndate          : ${date}`
-               error_message += `\nurl           : ${url}`
-               error_message += `\nJSON          : ${JSON.stringify(data, null, 2)}` // https://stackoverflow.com/q/5612787/9157799#comment53474797_5612849
-               error_message += '\n------------------------------'
-               throw error_message
-            }
-            return data
-         }
-
          const repo_number = await pgv.get('top_5_closed_pr_daily_fetch_count') + 1
          const repo_full_name = await get_repo_full_name(sql, repo_number)
          let num_of_closed_pr_since_1_year, top_5_closed_pr // https://stackoverflow.com/q/59416204/9157799
@@ -81,24 +60,6 @@ let task_fetch_github_api = Cron('*/9 * * * * *', { timezone: 'Etc/UTC' }, async
          if (repo_number % 400 == 0) console.log(`fetched top 5 closed PR (repo ${repo_number})`)
          await sql`UPDATE repository SET num_of_closed_pr_since_1_year = ${num_of_closed_pr_since_1_year}, total_thumbs_up_of_top_5_closed_pr_since_1_year = ${total_thumbs_up_of_top_5_closed_pr_since_1_year} WHERE id = ${repository_id};`
       } else if (await pgv.get('top_5_closed_issues_daily_fetch_count') < 1000) { // fetch top 5 CLOSED ISSUES and stuff
-         const fetch_top_5_closed_issues_since = async (repo_full_name, date) => { // fetch top 5 closed issues of the last 365 days
-            const url = `https://api.github.com/search/issues?sort=reactions-%2B1&per_page=5&q=state:closed%20type:issue%20closed:%3E${date}%20repo:${repo_full_name}`
-            const response = await fetch(url, github_api_fetch_options) // https://trello.com/c/aPVztlM3/8-fetch-api-get-top-5-closed-possibly-merged-prs-of-the-last-12-months
-            const data = await response.json()
-            if (!response.ok) { // https://stackoverflow.com/a/38236296/9157799
-               let error_message = 'ERROR'
-               error_message += '\n---------------------------------'
-               error_message += '\nfetch_top_5_closed_issues_since()'
-               error_message += `\nrepo_full_name: ${repo_full_name}`
-               error_message += `\ndate          : ${date}`
-               error_message += `\nurl           : ${url}`
-               error_message += `\nJSON          : ${JSON.stringify(data, null, 2)}` // https://stackoverflow.com/q/5612787/9157799#comment53474797_5612849
-               error_message += '\n---------------------------------'
-               throw error_message
-            }
-            return data
-         }
-
          const repo_number = await pgv.get('top_5_closed_issues_daily_fetch_count') + 1
          const repo_full_name = await get_repo_full_name(sql, repo_number)
          let num_of_closed_issues_since_1_year, top_5_closed_issues // https://stackoverflow.com/q/59416204/9157799
--- src/github_api.js
+++ src/github_api.js
@@ -6,6 +6,45 @@ export const fetch_repos = async (page) => {
    return data
 }
 
+export const fetch_top_5_closed_issues_since = async (repo_full_name, date) => { // fetch top 5 closed issues of the last 365 days
+   const url = `https://api.github.com/search/issues?sort=reactions-%2B1&per_page=5&q=state:closed%20type:issue%20closed:%3E${date}%20repo:${repo_full_name}`
+   const response = await fetch(url, github_api_fetch_options) // https://trello.com/c/aPVztlM3/8-fetch-api-get-top-5-closed-possibly-merged-prs-of-the-last-12-months
+   const data = await response.json()
+   if (!response.ok) { // https://stackoverflow.com/a/38236296/9157799
+      let error_message = 'ERROR'
+      error_message += '\n---------------------------------'
+      error_message += '\nfetch_top_5_closed_issues_since()'
+      error_message += `\nrepo_full_name: ${repo_full_name}`
+      error_message += `\ndate          : ${date}`
+      error_message += `\nurl           : ${url}`
+      error_message += `\nJSON          : ${JSON.stringify(data, null, 2)}` // https://stackoverflow.com/q/5612787/9157799#comment53474797_5612849
+      error_message += '\n---------------------------------'
+      throw error_message
+   }
+   return data
+}
+
+export const fetch_top_5_closed_PR_since = async (repo_full_name, date) => { // fetch top 5 closed PR of the last 365 days
+   const url = `https://api.github.com/search/issues?sort=reactions-%2B1&per_page=5&q=state:closed%20type:pr%20closed:%3E${date}%20repo:${repo_full_name}`
+   const response = await fetch(url, github_api_fetch_options) // https://trello.com/c/aPVztlM3/8-fetch-api-get-top-5-closed-possibly-merged-prs-of-the-last-12-months
+   const data = await response.json()
+   if (!response.ok) { // https://stackoverflow.com/a/38236296/9157799
+      // MAYBE TODO: When
+      //               "The listed users and repositories cannot be searched either because the resources do not exist or you do not have permission to view them."
+      //             then delete the repo from the database. Restarting the app won't remove it from the db unless it's been 24 hours. See clear_outdated_repos()
+      let error_message = 'ERROR'
+      error_message += '\n------------------------------'
+      error_message += '\nfetch_top_5_closed_PR_since()'
+      error_message += `\nrepo_full_name: ${repo_full_name}`
+      error_message += `\ndate          : ${date}`
+      error_message += `\nurl           : ${url}`
+      error_message += `\nJSON          : ${JSON.stringify(data, null, 2)}` // https://stackoverflow.com/q/5612787/9157799#comment53474797_5612849
+      error_message += '\n------------------------------'
+      throw error_message
+   }
+   return data
+}
+
 export const get_repo_new_name = async (repo_full_name) => {  // When the name of the repo or owner is changed, the search API can't detect the new name.
    const url = `https://api.github.com/repos/${repo_full_name}`
    const response = await fetch(url, github_api_fetch_options)
