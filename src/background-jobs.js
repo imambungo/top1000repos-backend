6666203f828078df87171cd6c4a896c25b3a0249
M Imam Pratama <imambungo@gmail.com>
2025-09-28 10:40:06 +0700

    s

--- src/background-jobs.js
+++ src/background-jobs.js
@@ -12,13 +12,7 @@ const pgv = persistent_global_variable(sql)
 
 import { send_to_telegram } from './lib/send_to_telegram.js'
 
-const githubApiVersion = '2022-11-28'
-const apiRequestHeaders = {  // https://trello.com/c/MgI1fvc5 | https://developer.mozilla.org/en-US/docs/Web/API/fetch#syntax
-   'X-GitHub-Api-Version': githubApiVersion,                 // https://docs.github.com/en/rest/overview/api-versions?apiVersion=2022-11-28#specifying-an-api-version
-   'User-Agent': 'imambungo',                                // https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#user-agent-required
-   'Authorization': `Bearer ${process.env.GITHUB_API_TOKEN}` // https://docs.github.com/en/rest/overview/authenticating-to-the-rest-api?apiVersion=2022-11-28
-}
-const github_api_fetch_options = {headers: apiRequestHeaders} // https://developer.mozilla.org/en-US/docs/Web/API/fetch#syntax
+import { github_api_fetch_options } from './github_api_fetch_options.js';
 
 let taskFetchGithubApi = Cron('*/9 * * * * *', { timezone: 'Etc/UTC' }, async () => {  // every 9 second | https://stackoverflow.com/a/59800039/9157799 | https://crontab.guru/
    if (G_fetch_quota > 0) {
