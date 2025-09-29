d738760a77380198d078124bdc622a0c957bf4b9
M Imam Pratama <imambungo@gmail.com>
2025-09-29 08:58:39 +0700

    s

--- src/lib/persistent_global_variable.js
+++ src/lib/persistent_global_variable.js
@@ -1,24 +1,4 @@
-// TEMPORARY
-const send_to_telegram = async (message) => { // https://core.telegram.org/bots/api#sendmessage
-	const requestBody = {
-		'chat_id': process.env.TELEGRAM_USER_ID,
-		'text': message,
-		'parse_mode': 'HTML' // https://core.telegram.org/bots/api#formatting-options | https://stackoverflow.com/a/49538689/9157799
-	}
-	try { // handle ConnectTimeoutError
-		const response = await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_API_TOKEN}/sendMessage`, {
-			 method: 'POST',
-			 body: JSON.stringify(requestBody),
-			 headers: { 'Content-Type': 'application/json' }
-		})
-		const data = await response.json()
-		if (!response.ok) { // if API/HTTP error | https://stackoverflow.com/a/38236296/9157799
-			console.log(`API error:\n${response.status} ${JSON.stringify(data, null, 2)}`) // https://stackoverflow.com/q/5612787/9157799#comment53474797_5612849
-		}
-	} catch (err) {
-		console.log(`Failed to fetch to telegram API:\n${err}`)
-	}
-}
+import { send_to_telegram } from "./send_to_telegram.js" // TEMPORARY
 
 let connection_is_ready = true // should throw an error if not ready for 30s: https://github.com/porsager/postgres?tab=readme-ov-file#connect_timeout
 const persistent_global_variable = sql => ({
