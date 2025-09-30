c882baf33dd32f54d13837ba422d93b0e401b09f
M Imam Pratama <imambungo@gmail.com>
2025-09-30 08:33:06 +0700

    s

--- src/get_repo_full_name.js
+++ src/get_repo_full_name.js
@@ -1,10 +1,14 @@
 export const get_repo_full_name = async (sql, repo_number) => {
-   const [{ full_name }] = await sql` -- https://github.com/porsager/postgres#usage
-      SELECT full_name
-         FROM (
-            SELECT row_number() OVER (ORDER BY stargazers_count DESC), full_name FROM repository
-         ) as stupid_alias  -- https://stackoverflow.com/q/14767209/9157799#comment56350360_14767216
-         WHERE row_number = ${repo_number};
-   `
-   return full_name
+   try {
+      const [{ full_name }] = await sql` -- https://github.com/porsager/postgres#usage
+         SELECT full_name
+            FROM (
+               SELECT row_number() OVER (ORDER BY stargazers_count DESC), full_name FROM repository
+            ) as stupid_alias  -- https://stackoverflow.com/q/14767209/9157799#comment56350360_14767216
+            WHERE row_number = ${repo_number};
+      `
+      return full_name
+   } catch (e) {
+      throw `Error in get_repo_full_name(sql, repo_number: ${repo_number}):\n${e.message}`
+   }
 }
